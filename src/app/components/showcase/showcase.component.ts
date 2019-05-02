b3a78cf2ab3c831b1599aaa797192e35450139aa
sumitlokhande <sumitlokhande858@gmail.com>
2019-05-02 16:25:55 +0530

    animate css added as cdn

--- src/app/components/showcase/showcase.component.ts
+++ src/app/components/showcase/showcase.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showcase.component.sass']
 })
 export class ShowcaseComponent implements OnInit {
+  url: string;
+  artForms =  [
+    { name : 'Dance'},
+    { name : 'Graffiti'},
+    { name : 'Music'},
+    { name : 'Photography'},
+    { name : 'Travel'},
+    { name : 'Music'},
+    { name : 'Photography'},
+    { name : 'Travel'},
 
-  constructor() { }
+  ]
+  constructor() {
+    this.url = 'https://images.unsplash.com/photo-1508700929628-666bc8bd84ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80';
+   }
 
   ngOnInit() {
   }
 
+  showDetails(name){
+    console.log(name);
+  }
 }
